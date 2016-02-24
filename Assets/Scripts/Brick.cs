c80483f6df461bd2c8488a7f9442f316fe278ae6
Marek Kisiel <mr.kislun@gmail.com>
2016-02-24 02:12:36 +0100

    First Commit
    The first version of my Arkanoid game, made in Unity 5

---
+++ Assets/Scripts/Brick.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Brick : MonoBehaviour {
+
+    public int maxHits;
+    public Sprite[] hitSprites;
+    public static int breakableCount = 0;
+
+    private int timesHit;
+    private LevelManager levelManager;
+    private bool isBreakable;
+
+    // Use this for initialization
+    void Start () {
+        isBreakable = (this.tag == "Breakable");
+
+        if (isBreakable)
+        {
+            breakableCount++;
+            print(breakableCount);
+        }
+
+        timesHit = 0;
+        levelManager = GameObject.FindObjectOfType<LevelManager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (isBreakable)
+        {
+            timesHit++;
+            maxHits = hitSprites.Length + 1;
+
+            if (timesHit >= maxHits)
+            {
+                breakableCount--;
+                print(breakableCount);
+                levelManager.BrickDestroyed();
+                Destroy(gameObject);
+            }
+            else
+            {
+                LoadSprites();
+            }
+        }
+        
+    }
+
+    void LoadSprites()
+    {
+        int spriteIndex = timesHit - 1;
+        if (hitSprites[spriteIndex])
+        {
+            this.GetComponent<SpriteRenderer>().sprite = hitSprites[spriteIndex];
+        }
+        
+
+    }
+        
+}
